48ee7d743d2af4c1d7d0e3d6f0128dd5f76f1a3a
ViktoriiaYakymovych <118008244+ViktoriiaYakymovych@users.noreply.github.com>
2023-04-20 10:27:33 +0300

    solved task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,1 +1,37 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesDiv = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+    const boxes = [];
+
+    for (let i = 0; i < amount; i += 1) {
+    const size = 30 + i * 10;
+    const box = document.createElement('div');
+
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+    }
+
+    boxesDiv.append(...boxes);
+}
+
+function destroyBoxes() {
+    boxesDiv.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+    createBoxes(Number(input.value));
+    input.value = '';
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
